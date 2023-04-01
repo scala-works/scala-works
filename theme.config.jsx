7e6abe7100cb20f2708877286a7fd6c84721a628
Mark Rudolph <mark@k8ty.app>
2023-03-31 22:23:38 -0400

    update site head metadata + footer

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,29 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
     logo: <span>ScalaWorks</span>,
     project: {
       link: 'https://github.com/scala-works/scala-works',
     },
     docsRepositoryBase: 'https://github.com/scala-works/scala-works/blob/main',
-    // ...
+   head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://scala.works' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return <>
+      <title>ScalaWorks</title>
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={frontMatter.title || 'ScalaWorks'} />
+      <meta property="og:description" content={frontMatter.description || 'A site about the Scala 3 programming language'} />
+    </>
+  },
+  footer: {
+    text: <span>
+      ScalaWorks site content is licensed the <a href="https://github.com/scala-works/scala-works/blob/main/LICENSE.md" target="_blank">Apache 2.0 License</a>.
+    </span>,
+  }, 
   }
